12650ac920974f5cad2f700e9a0724773770c19a
mistercapote <luizbravin0@gmail.com>
2026-09-19 19:24:11 -0300

    add product restriction

--- domain/builder.py
+++ domain/builder.py
@@ -38,6 +38,8 @@ class OrderBuilder:
     def build(self) -> Order:
         if not self._customer:
             raise ValueError("Não é possível construir um pedido sem cliente.")
+        if not self.products:
+            raise ValueError("O pedido deve conter pelo menos um produto.")
 
         return Order(
             customer=self._customer,
